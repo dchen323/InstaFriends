deba345856285526b5f041d15571a28979f048d4
Daniel <d.chen323@gmail.com>
2018-06-05 23:13:33 -0700

    Finish styling signup and login page

--- frontend/components/session/login_form.jsx
+++ frontend/components/session/login_form.jsx
@@ -42,24 +42,33 @@ class LoginForm extends React.Component {
   }
 
   render() {
+    const isEnabled = this.state.username.length > 0 &&
+      this.state.password.length > 0;
+    debugger
     return (
       <div className="login">
-        <h2>InstaFriends</h2>
-        <form className="form-box" onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.username}
-            onChange={this.update("username")} className="field"
-            placeholder="username"></input>
-          <input type="password" value={this.state.password}
-            onChange={this.update("password")} className="field"
-            placeholder="Password"></input>
-          <input type="submit" value="Log In"></input>
-        </form>
-        {this.renderErrors()}
-        <span className="have-account">
-          Have an account?
-          <Link to="/signup">Sign up</Link>
-        </span>
-        <button className="signup-button" onClick={this.handleGuest}>Log in with Demo User</button>
+        <div className="login-box">
+          <h2 className="title">InstaFriends</h2>
+          // <form className="form-box" onSubmit={this.handleSubmit}>
+            <input type="text" value={this.state.username}
+              onChange={this.update("username")} className="field"
+              placeholder="username"></input>
+            <input type="password" value={this.state.password}
+              onChange={this.update("password")} className="field"
+              placeholder="Password"></input>
+            <button className="signup-button" disabled={!isEnabled}
+              onClick={this.handleSubmit}>Log In</button>
+            <input type="submit" value="Log In" className="signup-button"></input>
+            <button className="signup-button" onClick={this.handleGuest}>Log in with Demo User</button>
+          </form>
+          {this.renderErrors()}
+        </div>
+          <div>
+            <div className="have-account">
+              <p>Don't an account?</p>
+              <Link to="/signup" className='login-redir'>Sign up</Link>
+            </div>
+          </div>
       </div>
     );
   }
--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -45,34 +45,32 @@ class SignupForm extends React.Component {
   render() {
     return(
       <div className="signup">
-        <div className="login-picture">
-          <img src='https://s20.postimg.cc/ltx8do2rx/Ig1.jpg'
-            className="login-picture1"></img>
-          <img src='https://s20.postimg.cc/4gmxywmx9/Ig2.jpg'
-            className="login-picture2"></img>
-        </div>
-        <div className="signup-form">
-          <h2>InstaFriends</h2>
-          <p className="signup-ad">Sign up to see photos and videos from your friends.</p>
-          <button className="signup-button" onClick={this.handleGuest}>Log in with Demo User</button>
-          <form className="form-box" onSubmit={this.handleSubmit}>
-            <input type="text" value={this.state.name}
-              onChange={this.update("name")} className="field"
-              placeholder="Full Name"></input>
-            <input type="text" value={this.state.username}
-              onChange={this.update("username")} className="field"
-              placeholder="Username"></input>
-            <input type="password" value={this.state.password}
-              onChange={this.update("password")} className="field"
-              placeholder="Password"></input>
-            <input type="submit" value="Sign Up" className="signup-button"></input>
-          </form>
-          {this.renderErrors()}
-          <p className="terms">By signing up, you agree to our Terms,Data Policy and Cookies Policy.</p>
-          <span className="have-account">
-              Have an account?
-              <Link to="/login">Log in</Link>
-            </span>
+        <img src='https://s20.postimg.cc/9lsqu622l/for_Daniel2.png'
+          className="login-picture1"></img>
+        <div class="signup-box">
+          <div className="signup-form">
+            <h2 className="title">InstaFriends</h2>
+            <button className="signup-button" onClick={this.handleGuest}>Log in with Demo User</button>
+            <form className="form-box" onSubmit={this.handleSubmit}>
+              <input type="text" value={this.state.name}
+                onChange={this.update("name")} className="field"
+                placeholder="Full Name"></input>
+              <input type="text" value={this.state.username}
+                onChange={this.update("username")} className="field"
+                placeholder="Username"></input>
+              <input type="password" value={this.state.password}
+                onChange={this.update("password")} className="field"
+                placeholder="Password"></input>
+              <input type="submit" value="Sign Up" className="signup-button"></input>
+              <br></br>
+              <p className="terms">By signing up, you agree to our Terms,Data Policy and Cookies Policy.</p>
+            </form>
+            {this.renderErrors()}
+          </div>
+          <div className="have-account">
+            <p>Have an account? </p>
+            <Link to="/login" className="login-redir"> Log in</Link>
+          </div>
         </div>
       </div>
     );
