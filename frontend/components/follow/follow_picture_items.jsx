378d32349be8428b44fa4e35ceb807c2ce61704d
Daniel <d.chen323@gmail.com>
2018-09-06 13:42:09 -0700

    Add unlike picture on double click feature

--- frontend/components/follow/follow_picture_items.jsx
+++ frontend/components/follow/follow_picture_items.jsx
@@ -1,50 +1,65 @@
-import React from 'react';
-import {FeedLikeItems} from '../likes/likes_feed_items';
-import CommentFormContainer from '../comment/comment_form_container';
-import CommentIndexContainer from '../comment/comment_index_container';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { FeedLikeItems } from "../likes/likes_feed_items";
+import CommentFormContainer from "../comment/comment_form_container";
+import CommentIndexContainer from "../comment/comment_index_container";
+import { Link } from "react-router-dom";
 
-export const FollowPictureItem = ({user, picture,
-  likes,handleLike, handleUnlike, setDate,currentUser}) => {
+export const FollowPictureItem = ({
+  user,
+  picture,
+  likes,
+  handleLike,
+  handleUnlike,
+  setDate,
+  currentUser
+}) => {
   let likeCount = likes.length;
-  let commentFeed ="comment-feed";
+  let commentFeed = "comment-feed";
+  let liked = likes.filter(like => like.userId === currentUser.id);
   return (
     <li className="picture-feed-container">
-        <span className="feed-user-pic">
+      <span className="feed-user-pic">
+        <Link to={`/user/${user.id}`}>
+          <img src={user.imgUrl} className="pic-show-userpic" />
+        </Link>
+        <Link to={`/user/${user.id}`}>
+          <h4 className="pic-show-username">{user.username}</h4>
+        </Link>
+      </span>
+      <div className="picture-holder-feed">
+        <img
+          src={picture.imgUrl}
+          className="picture-feed"
+          onDoubleClick={
+            liked.length ? handleUnlike(liked[0]) : handleLike(picture.id)
+          }
+        />
+      </div>
+      <br />
+      <div className="extra-info">
+        <FeedLikeItems
+          likes={likes}
+          likeCount={likeCount}
+          handleUnlike={handleUnlike}
+          handleLike={handleLike}
+          className="feed-likes"
+          pictureId={picture.id}
+          currentUserId={currentUser.id}
+        />
+        <span className="feed-user-caption">
           <Link to={`/user/${user.id}`}>
-            <img src={user.imgUrl} className="pic-show-userpic"/>
+            <h4 className="feed-username">{user.username}</h4>
           </Link>
-          <Link to={`/user/${user.id}`}>
-            <h4 className="pic-show-username">{user.username}</h4>
-          </Link>
+          <content>{picture.caption}</content>
         </span>
-        <div className="picture-holder-feed">
-          <img src={picture.imgUrl} className="picture-feed"
-            onDoubleClick={handleLike(picture.id)}/>
+        <div className="comment-feed">
+          <CommentIndexContainer picture={picture} commentFeed={commentFeed} />
         </div>
-        <br></br>
-        <div className="extra-info">
-          <FeedLikeItems likes={likes} likeCount={likeCount}
-            handleUnlike={handleUnlike}
-            handleLike={handleLike}
-            className="feed-likes"
-            pictureId={picture.id}
-            currentUserId={currentUser.id}/>
-          <span className="feed-user-caption">
-            <Link to={`/user/${user.id}`}>
-              <h4 className="feed-username">{user.username}</h4>
-            </Link>
-            <content>{picture.caption}</content>
-          </span>
-          <div className="comment-feed">
-            <CommentIndexContainer picture={picture}
-              commentFeed={commentFeed}/>
-          </div>
-          <span className="time-stamp">
-            {setDate(Date.parse(picture.createdAt))}
-          </span>
-          <CommentFormContainer pictureId={picture.id} />
-        </div>
+        <span className="time-stamp">
+          {setDate(Date.parse(picture.createdAt))}
+        </span>
+        <CommentFormContainer pictureId={picture.id} />
+      </div>
     </li>
   );
 };
--- frontend/components/picture/picture_index.jsx
+++ frontend/components/picture/picture_index.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import EditPictureFormContainer from './edit_picture_form_container';
-import CommentFormContainer from '../comment/comment_form_container';
-import CommentIndexContainer from '../comment/comment_index_container';
-import {LikeItems} from '../likes/likes_items';
-import {withRouter} from 'react-router';
-import {Link} from 'react-router-dom';
-import TimeAgo from 'javascript-time-ago';
-import en from 'javascript-time-ago/locale/en';
-
+import React from "react";
+import EditPictureFormContainer from "./edit_picture_form_container";
+import CommentFormContainer from "../comment/comment_form_container";
+import CommentIndexContainer from "../comment/comment_index_container";
+import { LikeItems } from "../likes/likes_items";
+import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
 
 class PictureIndex extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       modalIsOpen: true,
@@ -18,6 +17,9 @@ class PictureIndex extends React.Component {
     };
 
     this.setInputFocus = this.setInputFocus.bind(this);
+    this.handleLike = this.handleLike.bind(this);
+    this.handleUnlike = this.handleUnlike.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   handleDelete(e) {
@@ -26,66 +28,90 @@ class PictureIndex extends React.Component {
     this.props.closeModal();
   }
 
-  handleLike(e){
+  handleLike(e) {
     e.preventDefault();
     this.props.createLike(this.props.picture.id);
   }
 
-  handleUnlike(e){
+  handleUnlike(e) {
     e.preventDefault();
     this.props.deleteLike(this.props.likes);
   }
 
-  setInputFocus(value){
-    this.setState({inputFocus: value});
+  setInputFocus(value) {
+    this.setState({ inputFocus: value });
   }
 
-  setDate(date){
+  setDate(date) {
     TimeAgo.locale(en);
-    const timeAgo = new TimeAgo('en-US');
+    const timeAgo = new TimeAgo("en-US");
     return timeAgo.format(date);
   }
 
   render() {
-    if (this.props.picture === undefined){
-      return (<div></div>);
+    if (this.props.picture === undefined) {
+      return <div />;
     }
     let disabled;
-    if(this.props.picture.userId !== this.props.sessionId){
+    if (this.props.picture.userId !== this.props.sessionId) {
       disabled = "hide-button";
     }
 
-    return(
+    return (
       <div className="picture-show-container">
         <div className="display-picture-holder">
-          <img src={this.props.picture.imgUrl} className="display-picture"
-            onDoubleClick={this.handleLike.bind(this)}/>
+          <img
+            src={this.props.picture.imgUrl}
+            className="display-picture"
+            onDoubleClick={
+              this.props.likes ? this.handleUnlike : this.handleLike
+            }
+          />
         </div>
         <div className="pic-show-info">
           <header className="pic-show-header">
-            <Link to={`/user/${this.props.user.id}`}
-              onClick={this.props.closeModal}>
-              <img src={this.props.user.imgUrl} className="pic-show-userpic"/>
+            <Link
+              to={`/user/${this.props.user.id}`}
+              onClick={this.props.closeModal}
+            >
+              <img src={this.props.user.imgUrl} className="pic-show-userpic" />
             </Link>
-            <Link to={`/user/${this.props.user.id}`}
-              onClick={this.props.closeModal}>
+            <Link
+              to={`/user/${this.props.user.id}`}
+              onClick={this.props.closeModal}
+            >
               <h2 className="pic-show-username">{this.props.user.username}</h2>
             </Link>
-            <button className={`pic-show-button2 ${disabled}`}
-              onClick={this.handleDelete.bind(this)}>Delete Picture</button>
+            <button
+              className={`pic-show-button2 ${disabled}`}
+              onClick={this.handleDelete}
+            >
+              Delete Picture
+            </button>
           </header>
-          <EditPictureFormContainer picture={this.props.picture}
-            disabled={disabled} user={this.props.user}/>
-          <CommentIndexContainer picture={this.props.picture}
-            closeModal={this.props.closeModal}/>
-          <LikeItems handleLike={this.handleLike.bind(this)}
-            handleUnlike={this.handleUnlike.bind(this)}
-            likeCount = {this.props.likeCount}
-            likes ={this.props.likes}
-            setInputFocus={this.setInputFocus}/>
-          <span className="time-stamp">{this.setDate(Date.parse(this.props.picture.createdAt))}</span>
-          <CommentFormContainer pictureId={this.props.picture.id}
-            inputFocus={this.state.inputFocus}/>
+          <EditPictureFormContainer
+            picture={this.props.picture}
+            disabled={disabled}
+            user={this.props.user}
+          />
+          <CommentIndexContainer
+            picture={this.props.picture}
+            closeModal={this.props.closeModal}
+          />
+          <LikeItems
+            handleLike={this.handleLike}
+            handleUnlike={this.handleUnlike}
+            likeCount={this.props.likeCount}
+            likes={this.props.likes}
+            setInputFocus={this.setInputFocus}
+          />
+          <span className="time-stamp">
+            {this.setDate(Date.parse(this.props.picture.createdAt))}
+          </span>
+          <CommentFormContainer
+            pictureId={this.props.picture.id}
+            inputFocus={this.state.inputFocus}
+          />
         </div>
       </div>
     );
