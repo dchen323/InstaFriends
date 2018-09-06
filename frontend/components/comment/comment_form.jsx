4ce1078c0bfece556726a390a50a52208bd357d7
Daniel <d.chen323@gmail.com>
2018-09-06 13:47:30 -0700

    Fix binding of function

--- frontend/components/comment/comment_form.jsx
+++ frontend/components/comment/comment_form.jsx
@@ -1,46 +1,50 @@
-import React from 'react';
-import {merge} from 'lodash';
+import React from "react";
+import { merge } from "lodash";
 
 export default class CommentForm extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      body: '',
+      body: "",
       inputFocus: this.props.inputFocus
     };
+
+    this.update = this.update.bind(this);
   }
 
-  componentWillReceiveProps(nextProp){
-    if(nextProp.inputFocus){
+  componentWillReceiveProps(nextProp) {
+    if (nextProp.inputFocus) {
       this.myInp.focus();
     }
   }
 
-  update(e){
+  update(e) {
     e.preventDefault();
-    this.setState({body: e.target.value});
+    this.setState({ body: e.target.value });
   }
 
-  handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
-    let newState = merge({},this.state);
+    let newState = merge({}, this.state);
     newState.picture_id = this.props.pictureId;
     this.props.postComment(newState);
-    this.setState({body: ''});
+    this.setState({ body: "" });
   }
 
   render() {
-    return(
+    return (
       <div className="comments-form-container">
         <form className="comments-form" onSubmit={this.handleSubmit.bind(this)}>
-          <input type="text" className="comments-box" value={this.state.body}
-            onChange={this.update.bind(this)}
+          <input
+            type="text"
+            className="comments-box"
+            value={this.state.body}
+            onChange={this.update}
             placeholder="Add Comment..."
-            ref={ip => this.myInp = ip}/>
+            ref={ip => (this.myInp = ip)}
+          />
         </form>
       </div>
     );
   }
-
 }
