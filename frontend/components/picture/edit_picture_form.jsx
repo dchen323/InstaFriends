89a9c63f77678fb4a0a5c5662c79e6a635f76d16
Daniel <d.chen323@gmail.com>
2019-02-25 00:19:21 -0800

    Fix placeholder and text not lining up properly

--- frontend/components/picture/edit_picture_form.jsx
+++ frontend/components/picture/edit_picture_form.jsx
@@ -1,39 +1,48 @@
-import React from 'react';
+import React from "react";
 
 export default class EditForm extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = this.props.picture;
   }
 
-
-  updateCaption(e){
+  updateCaption(e) {
     e.preventDefault();
     this.props.updatePicture(this.state);
   }
 
-  update(e){
+  update(e) {
     e.preventDefault();
-    this.setState({caption: e.target.value});
+    this.setState({ caption: e.target.value });
   }
 
   render() {
     let outline;
-    if(!this.props.disabled){
+    if (!this.props.disabled) {
       outline = "outline";
     }
-    return(
+    return (
       <div>
-        <form onSubmit={this.updateCaption.bind(this)}
-          className="pic-show-edit">
-          <h2 className="pic-show-username-userpage">{this.props.user.username}</h2>
-          <textarea type="text" value={this.state.caption ? this.state.caption : ""}
+        <form
+          onSubmit={this.updateCaption.bind(this)}
+          className="pic-show-edit"
+        >
+          <h2 className="pic-show-username-userpage">
+            {this.props.user.username}
+          </h2>
+          <textarea
+            type="text"
+            value={this.state.caption ? this.state.caption : ""}
             onChange={this.update.bind(this)}
             className={`input-show-box ${outline}`}
             disabled={this.props.disabled}
-            placeholder="caption..."></textarea>
-          <input type="submit" value="Edit Caption"
-            className={`pic-show-button ${this.props.disabled}`}/>
+            placeholder="caption..."
+          />
+          <input
+            type="submit"
+            value="Save Caption"
+            className={`pic-show-button ${this.props.disabled}`}
+          />
         </form>
       </div>
     );
