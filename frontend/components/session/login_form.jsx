44e5ef56a721becbab20d57cd56ae06ca118c6b8
Daniel <d.chen323@gmail.com>
2019-03-11 17:34:53 -0700

    Refactor session sass files

--- frontend/components/session/login_form.jsx
+++ frontend/components/session/login_form.jsx
@@ -1,45 +1,46 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {merge} from 'lodash';
+import React from "react";
+import { Link } from "react-router-dom";
+import { merge } from "lodash";
 
 class LoginForm extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      username: '',
-      password: ''
+      username: "",
+      password: ""
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleGuest = this.handleGuest.bind(this);
   }
 
   update(field) {
-    return e => this.setState({[field]: e.target.value});
+    return e => this.setState({ [field]: e.target.value });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.removeErrors();
   }
 
   handleSubmit(e) {
     e.preventDefault();
     const user = merge({}, this.state);
-    this.props.login(user).then(() => this.props.history.push('/'));
+    this.props.login(user).then(() => this.props.history.push("/"));
   }
 
-  handleGuest(e){
+  handleGuest(e) {
     e.preventDefault();
-    const user = merge(this.state, {username: "gunther", password: 'password'});
-    this.props.login(user).then(() => this.props.history.push('/'));
+    const user = merge(this.state, {
+      username: "gunther",
+      password: "password"
+    });
+    this.props.login(user).then(() => this.props.history.push("/"));
   }
 
   renderErrors() {
-    return(
+    return (
       <ul className="login-errors">
         {this.props.errors.map((error, i) => (
-          <li key={`error-${i}`}>
-            {error}
-          </li>
+          <li key={`error-${i}`}>{error}</li>
         ))}
       </ul>
     );
@@ -51,36 +52,55 @@ class LoginForm extends React.Component {
       errorClass = "invalid";
     }
 
-    const isEnabled = this.state.username.length > 0 &&
-      this.state.password.length > 0;
+    const isEnabled =
+      this.state.username.length > 0 && this.state.password.length > 0;
     let disabled;
-    if (!isEnabled){
+    if (!isEnabled) {
       disabled = "disabled";
     }
     return (
-      <div className="login">
-        <div className="login-box">
+      <div className="login flex-col-null-center">
+        <div className="login-box flex-col-null-center">
           <h2 className="title">InstaFriends</h2>
-          <form className="form-box" onSubmit={this.handleSubmit}>
-            <input type="text" value={this.state.username}
+          <form
+            className="form-box flex-col-null-center"
+            onSubmit={this.handleSubmit}
+          >
+            <input
+              type="text"
+              value={this.state.username}
               onChange={this.update("username")}
               className={`field ${errorClass}`}
-              placeholder="username"></input>
-            <input type="password" value={this.state.password}
+              placeholder="username"
+            />
+            <input
+              type="password"
+              value={this.state.password}
               onChange={this.update("password")}
               className={`field ${errorClass}`}
-              placeholder="Password"></input>
-            <button className={`signup-button ${disabled}`} disabled={!isEnabled}>Log In</button>
-            <button className="signup-button" onClick={this.handleGuest}>Log in with Demo User</button>
+              placeholder="Password"
+            />
+            <button
+              className={`signup-button ${disabled}`}
+              disabled={!isEnabled}
+            >
+              Log In
+            </button>
+            <button className="signup-button" onClick={this.handleGuest}>
+              Log in with Demo User
+            </button>
           </form>
           {this.renderErrors()}
         </div>
-          <div>
-            <div className="have-account">
-              <p>Don't an account?</p>
-              <Link to="/signup" className="login-redir"> Sign up</Link>
-            </div>
+        <div>
+          <div className="have-account flex-row-center-center">
+            <p>Don't an account?</p>
+            <Link to="/signup" className="login-redir">
+              {" "}
+              Sign up
+            </Link>
           </div>
+        </div>
       </div>
     );
   }
--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -1,79 +1,105 @@
-import React from 'react';
-import {withRouter, Link} from 'react-router-dom';
-import {merge} from 'lodash';
+import React from "react";
+import { withRouter, Link } from "react-router-dom";
+import { merge } from "lodash";
 
 class SignupForm extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      username: '',
-      name: '',
-      password: ''
+      username: "",
+      name: "",
+      password: ""
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleGuest = this.handleGuest.bind(this);
   }
 
   update(field) {
-    return e => this.setState({[field]: e.target.value});
+    return e => this.setState({ [field]: e.target.value });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.removeErrors();
   }
 
   handleSubmit(e) {
     e.preventDefault();
     const user = merge({}, this.state);
-    this.props.signup(user).then(() => this.props.history.push('/'));
+    this.props.signup(user).then(() => this.props.history.push("/"));
   }
 
-  handleGuest(e){
+  handleGuest(e) {
     e.preventDefault();
-    const user = merge(this.state, {username: "gunther", password: 'password'});
+    const user = merge(this.state, {
+      username: "gunther",
+      password: "password"
+    });
     this.props.login(user);
   }
 
   renderErrors() {
-    return(
+    return (
       <ul className="login-errors">
         {this.props.errors.map((error, i) => (
-          <li key={`error-${i}`}>
-            {error}
-          </li>
+          <li key={`error-${i}`}>{error}</li>
         ))}
       </ul>
     );
   }
 
   render() {
-    return(
-      <div className="signup">
-        <img src='https://s20.postimg.cc/9lsqu622l/for_Daniel2.png'
-          className="login-picture1"></img>
-        <div className="signup-box">
-          <div className="signup-form">
+    return (
+      <div className="signup flex-row-center-center">
+        <img
+          src="https://s20.postimg.cc/9lsqu622l/for_Daniel2.png"
+          className="login-picture1"
+        />
+        <div className="signup-box flex-col-null-center">
+          <div className="signup-form flex-col-null-center">
             <h2 className="title">InstaFriends</h2>
-            <button className="signup-button" onClick={this.handleGuest}>Log in with Demo User</button>
-            <form className="form-box" onSubmit={this.handleSubmit}>
-              <input type="text" value={this.state.name}
-                onChange={this.update("name")} className="field"
-                placeholder="Full Name"></input>
-              <input type="text" value={this.state.username}
-                onChange={this.update("username")} className="field"
-                placeholder="Username"></input>
-              <input type="password" value={this.state.password}
-                onChange={this.update("password")} className="field"
-                placeholder="Password"></input>
-              <input type="submit" value="Sign Up" className="signup-button"></input>
-              <p className="terms">By signing up, you agree to our Terms,Data Policy and Cookies Policy.</p>
+            <button className="signup-button" onClick={this.handleGuest}>
+              Log in with Demo User
+            </button>
+            <form
+              className="form-box flex-col-null-center"
+              onSubmit={this.handleSubmit}
+            >
+              <input
+                type="text"
+                value={this.state.name}
+                onChange={this.update("name")}
+                className="field"
+                placeholder="Full Name"
+              />
+              <input
+                type="text"
+                value={this.state.username}
+                onChange={this.update("username")}
+                className="field"
+                placeholder="Username"
+              />
+              <input
+                type="password"
+                value={this.state.password}
+                onChange={this.update("password")}
+                className="field"
+                placeholder="Password"
+              />
+              <input type="submit" value="Sign Up" className="signup-button" />
+              <p className="terms">
+                By signing up, you agree to our Terms,Data Policy and Cookies
+                Policy.
+              </p>
             </form>
-          <br></br>
+            <br />
             {this.renderErrors()}
           </div>
-          <div className="have-account">
+          <div className="have-account flex-row-center-center">
             <p>Have an account? </p>
-            <Link to="/login" className="login-redir"> Log in</Link>
+            <Link to="/login" className="login-redir">
+              {" "}
+              Log in
+            </Link>
           </div>
         </div>
       </div>
